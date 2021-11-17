108c4888cf924603fb1060df38596d7d24e95234
nazstone <nazstone@gmail.com>
2021-11-17 19:15:01 +0100

    fix: package electron

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 import './index.css';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
   // eslint-disable-next-line no-undef
   document.getElementById('root')
