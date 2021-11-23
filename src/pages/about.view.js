c367669e21abbb250cde4fc844172d1e8fb68cfe
nazstone <nazstone@gmail.com>
2021-11-23 08:05:29 +0100

    fix: about icon change

--- src/pages/about.view.js
+++ src/pages/about.view.js
@@ -17,14 +17,16 @@ const AboutView = () => {
         </ul>
       </p>
       <p className="pt-2">
-        Icons made by{' '}
-        <a href="https://www.freepik.com" title="Freepik">
-          Freepik
-        </a>{' '}
-        from{' '}
-        <a href="https://www.flaticon.com/" title="Flaticon">
-          www.flaticon.com
-        </a>
+        <div>
+          Icons made by{' '}
+          <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">
+            Smashicons
+          </a>{' '}
+          from{' '}
+          <a href="https://www.flaticon.com/" title="Flaticon">
+            www.flaticon.com
+          </a>
+        </div>
       </p>
     </div>
   );
